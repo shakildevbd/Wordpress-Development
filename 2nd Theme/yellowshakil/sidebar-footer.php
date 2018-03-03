e16233db02b35bcc50884140c6dcb4ece1d9793b
shakildevbd <36607261+shakildevbd@users.noreply.github.com>
2018-03-04 00:39:17 +0600

    Developed
    Second Theme Developed

---
+++ 2nd Theme/yellowshakil/sidebar-footer.php
@@ -0,0 +1,7 @@
+<section id="footer_top_area">
+			<div class="clearfix wrapper footer_top">
+				<div class="clearfix footer_top_container">
+					<?php dynamic_sidebar('footer-sidebar'); ?>
+				</div>
+			</div>
+		</section>
---
+++ 2nd Theme/yellowshakil/sidebar.php
@@ -0,0 +1,5 @@
+<div class="clearfix sidebar_container floatright">
+			<div class="clearfix sidebar">
+					<?php dynamic_sidebar('right-sidebar'); ?>
+			</div>
+</div>
