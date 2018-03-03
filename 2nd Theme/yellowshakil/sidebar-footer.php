03f6e19b7eb71118fb25aafe9903950bf95f22d8
shakildevbd <36607261+shakildevbd@users.noreply.github.com>
2018-03-04 00:40:02 +0600

    delete
    delte

--- 2nd Theme/yellowshakil/sidebar-footer.php
+++
@@ -1,7 +0,0 @@
-<section id="footer_top_area">
-			<div class="clearfix wrapper footer_top">
-				<div class="clearfix footer_top_container">
-					<?php dynamic_sidebar('footer-sidebar'); ?>
-				</div>
-			</div>
-		</section>
--- 2nd Theme/yellowshakil/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div class="clearfix sidebar_container floatright">
-			<div class="clearfix sidebar">
-					<?php dynamic_sidebar('right-sidebar'); ?>
-			</div>
-</div>
